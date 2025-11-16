614a2e6f5d48690eecc75b666524b7bdfb9e7776
Aya-Gebril <aya2gebril@gmail.com>
2025-11-16 10:39:54 +0200

    Course class constructor

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -8,7 +8,17 @@ public class Course {
     private String title;
     private String description;
     private int instructorId;
-
     private List<Integer> students;
     private List<Lesson> lessons;
+
+    public Course(int courseId, String title, String description, int instructorId){
+        this.courseId = courseId;
+        this.title = title;
+        this.description = description;
+        this.instructorId = instructorId;
+        this.students = new ArrayList<>();
+        this.lessons = new ArrayList<>();
+    }
+
+
 }
