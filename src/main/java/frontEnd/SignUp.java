3b974fd48a8d1f89e529f9e8a3c8aa20069472fc
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-11-16 19:45:26 +0200

    Sign up

--- src/main/java/frontEnd/SignUp.java
+++ src/main/java/frontEnd/SignUp.java
@@ -150,7 +150,9 @@ public class SignUp extends javax.swing.JFrame {
     }//GEN-LAST:event_jComboBox1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        Login l = new Login();
+        l.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
