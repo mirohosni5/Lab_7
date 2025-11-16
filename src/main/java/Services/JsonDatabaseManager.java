d1fd7aca26701321d3fe9bfa2b846bdcd49f5d47
Aya-Gebril <aya2gebril@gmail.com>
2025-11-16 17:51:55 +0200

    JsonDatabaseManager attributes

--- src/main/java/Services/JsonDatabaseManager.java
+++ src/main/java/Services/JsonDatabaseManager.java
@@ -1,5 +1,15 @@
 package Services;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import models.Course;
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.Type;
+
 public class JsonDatabaseManager {
-    
+    private static final String COURSES_FILE = "courses.json";
+    private static final Gson gson = new Gson();
+
+
 }
