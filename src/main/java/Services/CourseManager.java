59d0075f1bfdcf1391ab01ba8b94672ec156ecab
Aya-Gebril <aya2gebril@gmail.com>
2025-11-16 19:00:52 +0200

    CourseManager (Create course)

--- src/main/java/Services/CourseManager.java
+++ src/main/java/Services/CourseManager.java
@@ -1,12 +1,23 @@
 package Services;
 
 import models.Course;
-import models.Lesson;
 
 import java.util.List;
 
 public class CourseManager {
-    private int generateCourseId() {
-        return (int)(Math.random() * 9000) + 1000;
+    private int generateCourseId(List<Course> courses) {
+        return (int) (Math.random() * 9000) + 1000;
     }
+
+    public Course createCourse(String title, String description, int instructorId) {
+        List<Course> courses = JsonDatabaseManager.readCourses();
+        int newId = generateCourseId(courses);
+
+        Course course = new Course(newId, title, description, instructorId);
+        courses.add(course);
+
+        JsonDatabaseManager.writeCourses(courses);
+        return course;
+    }
+
 }
