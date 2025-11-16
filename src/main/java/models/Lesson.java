0a3fe1b9efeab67779dced1cdf2f6a10640a9e39
Aya-Gebril <aya2gebril@gmail.com>
2025-11-16 10:49:24 +0200

    Lesson class constructor

--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson {
@@ -7,6 +8,13 @@ public class Lesson {
     private String title;
     private String content;
     private List<String> resources;
+
+    public Lesson(int lessonId, String title, String content, List<String> resources){
+        this.lessonId = lessonId;
+        this.title = title;
+        this.content = content;
+        this.resources = resources;
+    }
 
 
 }
