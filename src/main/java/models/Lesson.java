a01c161063cc03942c106e10aa3969788e75d4aa
Aya-Gebril <aya2gebril@gmail.com>
2025-11-16 10:46:13 +0200

    Lesson class attributes

--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.List;
+
 public class Lesson {
+    private int lessonId;
+    private String title;
+    private String content;
+    private List<String> resources;
+
+
 }
