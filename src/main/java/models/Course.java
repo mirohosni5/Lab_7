ba5caa37e645c8418f91eca128f3d91cad000b92
Aya-Gebril <aya2gebril@gmail.com>
2025-11-16 10:33:57 +0200

    start Course class

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+    private int courseId;
+    private String title;
+    private String description;
+    private int instructorId;
+
+
+
 }
